87b4c86e60927c57bdff1acb01f84568a081f7e6
Tatiana Garcia Pulgarin <tatiana.garcia@coins-colombia.com>
2023-11-23 20:42:47 -0500

    add navigate to home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from "./view/HomePage";
 import ViewOne from "./view/ViewOne";
 import ViewTwo from "./view/ViewTwo";
@@ -13,7 +13,7 @@ function App() {
         <Sidebar />
         <div style={{ marginLeft: '200px', padding: '20px' }}>
       <Routes>
-        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/home-page" element={<Navigate to="/home-page" />} />
         <Route path="/view-one" element={<ViewOne />} />
         <Route path="/view-two" element={<ViewTwo />} />
       </Routes>
